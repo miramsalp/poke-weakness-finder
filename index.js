796f15a0bc0919e937e73cfdba02bea63acd9426
miramsalp <Thanapat_petch@hotmail.com>
2025-06-10 14:02:46 +0700

    add navbar

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
