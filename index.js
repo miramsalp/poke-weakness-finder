651219949ccfd34a318760c489d50fc27d4420bc
miramsalp <Thanapat_petch@hotmail.com>
2025-06-10 14:33:22 +0700

    add pokemon fetch function

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ import express from "express"
 import axios from "axios"
 import bodyParser from "body-parser"
 import { getAllTypeEffectiveness } from "./calculate.js";
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+let allPokemonNames = []; // To store all Pokemon names
+async function fetchAllPokemonNames() {
+    try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1500"); 
+        allPokemonNames = response.data.results.map(p => p.name);
+        console.log(`Fetched ${allPokemonNames.length} Pokemon names.`);
+        // console.log(allPokemonNames[0]);
+    } catch (error) {
+        console.error("Error fetching all Pokemon names:", error.message);
+    }
+}
+fetchAllPokemonNames();
+// console.log(allPokemonNames[0]);
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
